074eaf047e52e903dad5558c2ef72956b67b8564
Dylan Harvey <dharveyasp@gmail.com>
2023-04-20 15:29:32 -0400

    Added html tables and updated site visuals. Added homepage.

--- users/urls.py
+++ users/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     # users/register/
-    path('register/', views.Register.as_view(), name="register"),
+    path('register/', views.Register.as_view(), name="users_register"),
     # users/login/
-    path('login/', views.Login.as_view(), name="login"),
+    path('login/', views.Login.as_view(), name="users_login"),
     # users/logout/
-    path('logout/', views.Logout.as_view(), name="logout"),
+    path('logout/', views.Logout.as_view(), name="users_logout"),
     # Adds a bunch of URLs (users/login, etc.)
     path('', include('django.contrib.auth.urls')),
 ]
